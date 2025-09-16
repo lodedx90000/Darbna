b130a81a1de290c1dbb3bcd2407d77419c177534
Adel Faisal <anklet-lobster0v@icloud.com>
2025-09-16 19:27:41 +0000

    PWA

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE = 'pwa-demo-v1'; // bump to v2 for the update demo
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192x192.jpg',
+  './icons/icon-512x512.jpg'
+];
+
+self.addEventListener('install', (e) => {
+  // make new SW active ASAP
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  // claim clients so updates are immediate
+  e.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)));
+    await self.clients.claim();
+  })());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
